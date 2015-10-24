e1cb1e3b1359ef5e925aeeb22b48a8947abedc27
pritic <changlani.priti@gmail.com>
2015-10-24 18:17:37 -0400

    search implemented; avg hops displayed

--- src/main/scala/Interval.scala
+++ src/main/scala/Interval.scala
@@ -1,39 +1,36 @@
 /**
  * Created by PRITI on 10/23/15.
  */
-class Interval(leftInclude: Boolean, leftValue: BigInt, rightValue: BigInt,
-        rightInclude: Boolean) {
+class Interval(
+                leftInclude: Boolean, leftValue: BigInt, rightValue: BigInt,
+                rightInclude: Boolean) {
 
   def includes(value: BigInt): Boolean = {
 
     if (leftValue == rightValue) {
-      if (leftInclude == false && rightInclude == false && value == leftValue) {
-        return false
-      }
-      else {
-        return true
-      }
+      if (!leftInclude && !rightInclude && value == leftValue)
+        false
+      else
+        true
     }
     else if (leftValue < rightValue) {
-      if ((value == leftValue && leftInclude == true) || (value == rightValue && rightInclude == true) || (value > leftValue && value < rightValue)) {
-        return true
-      } else {
-        return false
-      }
+      if (value == leftValue && leftInclude || value == rightValue && rightInclude || (value > leftValue && value < rightValue))
+        true
+      else
+        false
     }
     else {
-      if ((value == leftValue && leftInclude == true) || (value == rightValue && rightInclude == true) || (value > leftValue || value < rightValue)) {
-        return true
-      } else {
-        return false
-      }
+      if (value == leftValue && leftInclude || value == rightValue && rightInclude || (value > leftValue || value < rightValue))
+        true
+      else
+        false
     }
 
   }
 
-  def getEnd(): BigInt = {
+  def getEnd: BigInt = {
 
-    return rightValue
+    rightValue
   }
 
 }
--- src/main/scala/project3.scala
+++ src/main/scala/project3.scala
@@ -1,14 +1,26 @@
 
-import akka.actor.{ActorRef, Actor, Props, ActorSystem}
 
-import scala.collection.immutable
+import java.util.concurrent.ConcurrentHashMap
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+
+import scala.collection._
+import scala.collection.convert.decorateAsScala._
 import scala.collection.immutable.HashSet
 import scala.math._
+import scala.util.Random
 
 /**
  * Created by PRITI on 10/17/15.
  */
 object project3 {
+
+  //  val calculationMap: concurrent.Map[Int, Int] = new ConcurrentHashMap()[Int, Int].asScala
+  val hopMap: concurrent.Map[String, Array[Int]] = new
+      ConcurrentHashMap[String,
+        Array[Int]]().asScala;
+  //key:number of nodes (Array[0]):number of
+  // hops (Array[1])
 
   var m: Int = 0;
 
@@ -18,11 +30,12 @@ object project3 {
     var numNodes: Int = args(0).toInt;
     var numRequests: Int = args(1).toInt;
     val system = ActorSystem("Chord")
-        m = 2 * log2(numNodes)
+//    m = 2 * log2(numNodes)
     //    println(m)
-//    m = log2(numNodes)
-//    m=3
-    var networkIPList: Set[String] = Set();
+//        m = log2(numNodes)
+        m=3
+    var networkIPList: Set[String] = new scala
+    .collection.mutable.HashSet[String];
     var IDList: Set[String] = Set();
     var id: String = ""
     var temp_id: String = ""
@@ -34,6 +47,7 @@ object project3 {
     var firstNode = system.actorOf(Props(new Node()), name = "0")
 
     ActorMap += firstNode
+    var k: Int = Random.nextInt(ActorMap.size)
 
     for (i <- 1 until numNodes) {
       while (IDList.contains(id)) {
@@ -45,23 +59,40 @@ object project3 {
 
       ActorMap += node
       Thread.sleep(100)
+      //      node ! Join(ActorMap.last)
       node ! Join(firstNode)
     }
 
-//    var node1 = system.actorOf(Props(new Node()), name = "1")
-//    ActorMap+=node1
-//    var node2 = system.actorOf(Props(new Node()), name = "3")
-//    ActorMap+=node2
+    //    var node1 = system.actorOf(Props(new Node()), name = "1")
+    //    ActorMap+=node1
+    //    var node2 = system.actorOf(Props(new Node()), name = "3")
+    //    ActorMap+=node2
 
-//    node1 ! Join(firstNode)
-//    Thread.sleep(100)
-//    node2 ! Join(firstNode)
-//    Thread.sleep(1000)
+    //    node1 ! Join(firstNode)
+    //    Thread.sleep(100)
+    //    node2 ! Join(firstNode)
+    //    Thread.sleep(1000)
 
     for (ar <- ActorMap) {
       Thread.sleep(100)
       ar ! Print
     }
+
+//    firstNode ! Find(firstNode, "11", 0)
+    for (ar <- ActorMap) {
+      ar ! Find(ar, "11", 0)
+    }
+
+    Thread.sleep(1000)
+    val avgHops = hopMap.get("5")
+
+    avgHops match {
+      case Some (x) =>
+        println("no of nodes that searched: "+x(0))
+        println("no of total hops: "+x(1))
+        println("average hops: "+(x(1).toDouble/x(0).toDouble))
+    }
+
   }
 
   def log2(x: Int): Int = {
@@ -81,7 +112,7 @@ object project3 {
     BigInt(hexValue, 16).toString(2)
   }
 
-  def getnetworkIp(networkIPList: immutable.Set[String]): String = {
+  def getnetworkIp(networkIPList: Set[String]): String = {
 
     val ip = scala.util.Random.nextInt(256) + "." + scala.util.Random.nextInt(256) + "." + scala.util.Random.nextInt(256) + "." + scala.util.Random.nextInt(256)
     if (!networkIPList.contains(ip))
@@ -103,21 +134,20 @@ object project3 {
     for (i <- 0 until m) {
       val start = (self.path.name.toInt + BigInt(2).pow(i)) % (BigInt(2).pow(m))
       val end = (self.path.name.toInt + BigInt(2).pow(i + 1)) % (BigInt(2).pow(m))
-      val range = new Interval(true, start, end, false)
-      fingerTable(i) = new FingerEntry(start, range, self)
+      val interval = new Interval(true, start, end, false)
+      fingerTable(i) = new FingerEntry(start, interval, self)
     }
 
     def receive: Receive = {
 
       case Join(exist: ActorRef) => {
         println("join started for: " + self.path.name.toInt)
-        println("m: " + m)
+//        println("m: " + m)
         this.exist = exist
         //println("I am %S, I am asking %s to find my position".format(getHash,exist.toString().charAt(25)))
         exist ! Find_Predecessor(self, self.path.name.toInt)
       }
 
-      //
       case Find_Predecessor(node: ActorRef, id: Int) => {
         println("Find_Predecessor started")
         val interval = new Interval(false, self.path.name.toInt, fingerTable(0)
@@ -125,12 +155,11 @@ object project3 {
           true)
 
         //if the id is contained in the interval
-        if (interval.includes(id))
-          {
-            println("calling: "+self.path.name+" id: "+id)
+        if (interval.includes(id)) {
+          println("calling: " + self.path.name + " id: " + id)
           node ! Found_Position(self, this.successor)
-        //else find the closest preceeding finger and recurse
-             }
+          //else find the closest preceeding finger and recurse
+        }
         else {
 
           val target = closest_preceding_finger(id)
@@ -197,6 +226,49 @@ object project3 {
             target ! Update_Finger(before, i, node, nodeHash)
           }
         }
+      }
+
+      case Find(node: ActorRef, code: String, step: Int) => {
+        //        def id=getHash(code)
+        var keyHash = getHash(code)
+        var id = BigInt(keyHash.substring(0, m), 2).toString(10)
+        val interval = new Interval(false, self.path.name.toInt, fingerTable(0)
+          .getHash(), true)
+
+        if (interval.includes(id.toInt)) {
+          var arrayMap: Array[Int] = null
+          val result = hopMap.get(id)
+
+          result match {
+            case None =>
+              arrayMap = new Array[Int](2)
+              arrayMap(0) = 1
+              arrayMap(1) = step
+              hopMap.put(id, arrayMap)
+
+            case Some(x) =>
+              x(0) = x(0) + 1
+              x(1) = x(1) + step
+              hopMap.put(id, x)
+          }
+
+          node ! Found(code, self, successor, self.path.name.toInt, fingerTable(0).getHash(), step)
+        } else {
+          val target = closest_preceding_finger(id.toInt)
+          target ! Find(node, code, step + 1)
+        }
+      }
+
+      case Found(code: String, predecessor: ActorRef, successor: ActorRef, preHash: BigInt, sucHash: BigInt, step: Int) => {
+        var keyHash = getHash(code)
+        var id = BigInt(keyHash.substring(0, m), 2).toString(10)
+
+        println(("I am node %s, searching for %s with hash: %s and found " +
+          "it on node %s with successor: %s, predecessor: %s, using %s steps").format(self.path.name,code, id.toInt, successor.path.toString, sucHash, preHash, step))
+        val interval = new Interval(false, preHash, sucHash, true)
+
+        if (interval.includes(id.toInt))
+          println("The result is right!")
       }
 
       case Print => {
@@ -272,8 +344,15 @@ object project3 {
 
   case class Found_Finger(i: Int, successor: ActorRef)
 
-  case class Update_Finger(before: BigInt, i: Int, node: ActorRef, nodeHash:
+  case class Update_Finger(
+                            before: BigInt, i: Int, node: ActorRef, nodeHash:
   BigInt)
+
+  case class Find(node: ActorRef, code: String, step: Int)
+
+  case class Found(
+                    code: String, predecessor: ActorRef, successor: ActorRef,
+                    preHash: BigInt, sucHash: BigInt, step: Int)
 
   case class Print()
 
